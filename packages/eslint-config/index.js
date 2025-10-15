0defaf0c53646df5b139a5e3b4a57d39fe2ffc45
Amir Sarsen <amir@sarsen.dev>
2025-10-15 22:13:15 +0200

    feat(config): setup shared eslint and types placeholder

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "turbo"],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  root: true,
+};

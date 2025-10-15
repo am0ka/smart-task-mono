97ef3017b556c9f30fabba5aa18a2060d24e0737
Amir Sarsen <amir@sarsen.dev>
2025-10-15 23:42:20 +0200

    feat(prettier): add prettier for code formatting

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "turbo"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "turbo", "prettier"],
   plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   root: true,
